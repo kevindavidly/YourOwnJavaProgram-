5a31edad40d26f8f48e1227825bf45766c15fd90
kevindavidly <kevindavidly@gmail.com>
2019-08-05 16:41:32 -0700

    program

---
+++ YourOwnJavaProgram.java
@@ -0,0 +1,7 @@
+public class YourOwnJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Kevin Ly");
+        System.out.println("I am 27 years old");
+        System.out.println("My hometown is Oakland, CA");
+    }
+}
